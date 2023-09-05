2e3a17e2eda1a81eb2eff532ed3752862f642aa8
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2023-09-05 14:30:49 +0800

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Card from "$lib/components/card.svelte"
+    import portrait from "$lib/assets/images/portraits/portrait.jpg" 
+    import Person from '$lib/classes/person'
+
+    let personList: Person[] = [
+        new Person(
+            "Bluth, George", 
+            "george.bluth@reques.in", 
+            portrait
+        ), 
+    ]
+
+</script>
+
+<div class="flex h-screen flex-col gap-5 items-center content-center justify-center p-10 lg:p-20">
+    <div class="flex items-center content-center justify-center">
+            {#each personList as person, index}
+                <Card 
+                    avatar="{person.avatar}"
+                    id="{(index + 1).toString()}" 
+                    name="{person.name}"
+                    email="{person.email}"
+                />
+            {/each}
+    </div>
+
+</div>
+
+
+
